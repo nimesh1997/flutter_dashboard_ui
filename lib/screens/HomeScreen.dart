371d5c7c7724c977c2f7254ac1521220e8d1281c
nimesh <nkumarsachan@gmail.com>
2019-11-21 12:59:27 +0530

    minor bugs fixed

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -264,7 +264,7 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
           Padding(
-            padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+            padding: const EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0, top: 5.0),
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
